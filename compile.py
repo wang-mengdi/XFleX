795585f602928205a71c867d08a0ab82486c7151
wmdcstdio <wmdcstdio@pku.edu.cn>
2022-08-16 13:28:33 -0400

    compile_linux

--- compile.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import platform
-
-if __name__=='__main__':
-    compile_cmd="xmake build -P ./demo flex-demo"
-    print(compile_cmd)
-    os.system(compile_cmd)
---
+++ compile_linux.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import platform
+
+if __name__=='__main__':
+    proj_name="flex-demo"
+    bin_dir=os.path.join('..','bin')
+    build_dir=os.path.join('..','build')
+    lua_file=os.path.join('demo','xmake.lua')
+    proj_dir=os.path.join('demo')
+    clean_cmd="xmake c -P {} -a".format(proj_dir)
+    config_cmd="xmake f -o {} -P {} -y".format(bin_dir, proj_dir)
+    compile_cmd="xmake build -P {} {}".format(proj_dir,proj_name)
+    print(clean_cmd)
+    os.system(clean_cmd)
+    print(config_cmd)
+    os.system(config_cmd)
+    print(compile_cmd)
+    os.system(compile_cmd)
