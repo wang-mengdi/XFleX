0bfd5a576ef8d51bf45295de59e934be07095dc9
wmdcstdio <mengdi.wang.gr@dartmouth.edu>
2022-08-16 01:42:07 -0400

    some wteak

--- demo/xmake.lua
+++ demo/xmake.lua
@@ -14,7 +14,7 @@ target("flex-demo")
     add_includedirs("opengl",{public=true})
     add_headerfiles("(opengl/*.h)")
 
-    add_includedirs("(../core)",{public=true})
+    add_includedirs("../core",{public=true})
     add_headerfiles("(../core/*.h)")
 
     add_files("*.cpp","*.cu","*.cxx","*.cc")
@@ -37,14 +37,14 @@ target("flex-demo")
     --add_headerfiles("(d3d12/*.h)")
     --add_files("d3d12/*.cpp")
 
-    add_includedirs("(../external/GFSDK_Aftermath_v1.21/include)",{public=true})
+    add_includedirs("../external/GFSDK_Aftermath_v1.21/include",{public=true})
     add_headerfiles("(../external/GFSDK_Aftermath_v1.21/include/*.h)")
     add_files("../external/GFSDK_Aftermath_v1.21/lib/x64/*.lib")
 
 
-    add_includedirs("(../include)",{public=true})
+    add_includedirs("../include",{public=true})
     add_headerfiles("(../include/*.h)")
-    add_includedirs("(../lib/win64)",{public=true})
+    add_includedirs("../lib/win64",{public=true})
     add_files("../lib/win64/NvFlexReleaseCUDA_x64.lib","../lib/win64/NvFlexExtReleaseCUDA_x64.lib","../lib/win64/NvFlexDeviceRelease_x64.lib")
     
 
