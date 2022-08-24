b40033a72640d551a3a8c2c7f530c604f52d5912
wmdcstdio <mengdi.wang.gr@dartmouth.edu>
2022-08-23 20:59:44 -0400

    create a self function

--- demo/shaders.h
+++ demo/shaders.h
@@ -202,6 +202,7 @@ int GetNumDiffuseRenderParticles(DiffuseRenderBuffers* buffers);
 
 // screen space fluid rendering
 void RenderEllipsoids(FluidRenderer* render, FluidRenderBuffers* buffers, int n, int offset, float radius, float screenWidth, float screenAspect, float fov, Vec3 lightPos, Vec3 lightTarget, Matrix44 lightTransform, ShadowMap* shadowTex, Vec4 color, float blur, float ior, bool debug);
+void RenderEllipsoidsGL(FluidRenderer* render, FluidRenderBuffers* buffersIn, int n, int offset, float radius, float screenWidth, float screenAspect, float fov, Vec3 lightPos, Vec3 lightTarget, Matrix44 lightTransform, ShadowMap* shadowMap, Vec4 color, float blur, float ior, bool debug);
 void RenderDiffuse(FluidRenderer* render, DiffuseRenderBuffers* buffer, int n, float radius, float screenWidth, float screenAspect, float fov, Vec4 color, Vec3 lightPos, Vec3 lightTarget, Matrix44 lightTransform, ShadowMap* shadowTex, float motionBlur,  float inscatter, float outscatter, bool shadow, bool front);
 
 // UI rendering
